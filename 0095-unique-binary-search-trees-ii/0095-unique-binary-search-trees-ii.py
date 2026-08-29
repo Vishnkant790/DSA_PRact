ad55b76984cc6859b1c5f6ffa3ebcc5e417ff18c
Vishnukant <2k22csds.ng2.2213084@gmail.com>
2026-08-30 04:56:18 +0530

    Time: 3 ms (55.49%), Space: 20.3 MB (51.1%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -2,22 +2,13 @@ class Solution:
     def generateTrees(self, n: int):
         
         def generate(start, end):
-            # Empty subtree
             if start > end:
                 return [None]
 
             result = []
-
-            # Every value can be root
             for root_val in range(start, end + 1):
-
-                # Generate all possible left subtrees
                 left_trees = generate(start, root_val - 1)
-
-                # Generate all possible right subtrees
                 right_trees = generate(root_val + 1, end)
-
-                # Combine every left with every right
                 for left in left_trees:
                     for right in right_trees:
 
