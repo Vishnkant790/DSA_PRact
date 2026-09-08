baa84a506e841738aefe2ff8b5462c83459fcb37
Vishnukant <2k22csds.ng2.2213084@gmail.com>
2026-09-08 06:17:24 +0530

    Time: 1362 ms (25.05%), Space: 45.1 MB (16.11%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -6,8 +6,8 @@ class Solution:
         for i in range(1,n+1):
             for j in range(1,m+1): 
 
-                if s1[i-1] != s2[j-1]:
-                    continue
-                else:
+                if s1[i-1] == s2[j-1]:
                     t[i][j] =  1 + t[i-1][j-1]
+                else:
+                    t[i][j] =  0
         return max(max(row) for row in t)
